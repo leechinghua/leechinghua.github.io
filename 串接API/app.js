49b546e9fd7625919cb7f6d9dc9322e0be8b14e1
leechinghua <leechenghua107@gmail.com>
2025-03-05 18:03:26 +0800

    設定setTimeout鬧鐘響了

--- 串接API/app.js
+++ 串接API/app.js
@@ -1,9 +1,14 @@
-const name = document.querySelector('#name');
-const number = document.querySelector('#number');
+const personName = document.querySelector("#name");
+const delay = document.querySelector("#delay");
 const setAlarm = document.querySelector("#set-alarm");
 const output = document.querySelector("#output");
 
+function alarm(person, delayTime) {
+  setTimeout(() => {
+    output.innerHTML = person + "鬧鐘響了!";
+  }, delayTime);
+}
 
-setAlarm.addEventListener('click', (e) => {
-  console.log("你正在按下鬧鐘")
+setAlarm.addEventListener("click", (e) => {
+  alarm(personName.value, delay.value);
 });
