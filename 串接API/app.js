9c583f0a33361c285a46c09ea213cb863415bd88
leechinghua <leechenghua107@gmail.com>
2025-03-05 17:35:38 +0800

    建立鬧鐘基本格式

---
+++ 串接API/app.js
@@ -0,0 +1,9 @@
+const name = document.querySelector('#name');
+const number = document.querySelector('#number');
+const setAlarm = document.querySelector("#set-alarm");
+const output = document.querySelector("#output");
+
+
+setAlarm.addEventListener('click', (e) => {
+  console.log("你正在按下鬧鐘")
+});
