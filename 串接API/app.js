519468842909957f1f53c68f7c236be808872bc5
leechinghua <leechenghua107@gmail.com>
2025-03-05 18:25:44 +0800

    將setTimeout放入promise中，並且resolve出去

--- 串接API/app.js
+++ 串接API/app.js
@@ -4,11 +4,23 @@ const setAlarm = document.querySelector("#set-alarm");
 const output = document.querySelector("#output");
 
 function alarm(person, delayTime) {
-  setTimeout(() => {
-    output.innerHTML = person + "鬧鐘響了!";
-  }, delayTime);
+  return new Promise((resolve, reject) => {
+    if (person === "" || delayTime === "") {
+      reject("請填名字跟時間");
+    } else {
+      setTimeout(() => {
+        resolve(person + "，時間到了！");
+      }, delayTime);
+    }
+  });
 }
 
 setAlarm.addEventListener("click", (e) => {
-  alarm(personName.value, delay.value);
+  alarm(personName.value, delay.value)
+    .then((res) => {
+      output.innerHTML = res;
+    })
+    .catch((err) => {
+      output.innerHTML = err;
+    });
 });
